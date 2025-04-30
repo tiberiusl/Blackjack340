d0db1b4f209f49360b84cf1621b0e2c2c51db372
TK <tktranpham@gmail.com>
2025-04-29 21:47:38 -0700

    Adding to some classes to implement takeTurn() of Deck

---
+++ test.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include "linkedlist.h"
+#include "spade.h"
+#include "clover.h"
+#include "diamond.h"
+#include "heart.h"
+#include "deck.h"
+using namespace std;
+
+int main() {
+    //testing deck
+    cout << "testing deck creation" << endl;
+    Deck* newDeck = new Deck();
+    cout << "Expected deck size is 0, actual size: " << newDeck->size() << endl;
+    newDeck->takeTurn();
+    cout << "takeTurn() used, expected size is now 52, actual size: " << newDeck->size() << endl;
+    return 0;
+}
