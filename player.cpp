f4041f6d1fc1dfe550ed1c84422fd312abb1fcba
TK <tktranpham@gmail.com>
2025-05-05 21:54:44 -0700

    added basic implementation of player takeTurn()

--- player.cpp
+++ player.cpp
@@ -1,4 +1,5 @@
 #include "player.h"
+using namespace std;
 /*
 Player::Player() {
     this->hand = new LinkedList;
@@ -11,6 +12,26 @@ Player::~Player() {
 }
 */
 void Player::takeTurn(Deck& myDeck) {
-    
+    int playerNum;
+    int handVal = this->calculateHandValue();
+    cout << "Your current hand value: " << handVal << endl;
+    this->showCards();
+    bool stand = false;
+    while (!stand && handVal < 22) { //while handVal is less than 22 or player hasn't chosen stand
+        cout << "Enter 1 to hit (add a card to hand) or 0 to stand (end turn): ";
+        cin >> playerNum;
+
+        if (playerNum == 1) {
+            this->addCard(myDeck.pop_back()); //pop card from deck and add to hand
+        } else if (playerNum == 0) {
+            stand = true;
+        } else {
+            continue;
+        }
+
+        //update handVal
+        handVal = this->calculateHandValue();
+    }
+
 // to implement
 }
