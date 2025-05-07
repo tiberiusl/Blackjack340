24a6caa137f14d87b4c2a4b042785e5a4bb5eff2
TK <tktranpham@gmail.com>
2025-05-06 21:24:31 -0700

    added part of playGame() and startGame() of Blackjack class

--- blackjack.cpp
+++ blackjack.cpp
@@ -4,7 +4,9 @@
 using namespace std;
 
 BlackJack::BlackJack() {
-
+    this->player = Player();
+    this->dealer = Dealer();
+    this->deck = Deck();
 //to implement    
 }
 
@@ -14,11 +16,19 @@ BlackJack::~BlackJack() {
 }
 
 void BlackJack::playGame() {
-
+    this->player.takeTurn(this->deck);
+    this->dealer.takeTurn(this->deck);
 //to implement    
 }
 
 void BlackJack::startGame() {
+    this->deck.fill(); //fill deck
+    //shuffle deck
+
+    for (unsigned int i = 0; i < 2; ++i) { //add two cards to dealer + player
+        this->player.addCard(this->deck.pop_back());
+        this->dealer.addCard(this->deck.pop_back());
+    }
 
 //to implement    
 }
