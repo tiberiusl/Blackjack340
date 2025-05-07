bb90db5148a1d3dda0aad6d68c697985c588c5a1
TK <tktranpham@gmail.com>
2025-05-06 21:18:38 -0700

    added basic takeTurn() of dealer

--- dealer.cpp
+++ dealer.cpp
@@ -11,6 +11,10 @@ Dealer::~Dealer() {
 }
 */
 void Dealer::takeTurn(Deck& myDeck) {
-
+    int handVal = this->calculateHandValue();
+    while (handVal < 18) { //hit until 17 or over
+        this->addCard(myDeck.pop_back());
+        handVal = this->calculateHandValue();
+    }
 //to implement
 }
